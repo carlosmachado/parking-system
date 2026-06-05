e584c5f9cdb6a75a0acd33a6e5a1514642d7a58d
Carlos Machado <carlos.machado@lmagil.dev>
2026-06-05 02:12:44 -0300

    chore: bootstrap Spring Boot + Maven project with MySQL and Docker Compose

---
+++ src/main/java/br/com/cmachado/parkingsystem/ParkingSystemApplication.java
@@ -0,0 +1,14 @@
+package br.com.cmachado.parkingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class ParkingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingSystemApplication.class, args);
+    }
+}
